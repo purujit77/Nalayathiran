618773f0f8642fc41df2b0e1019068c56d91e77e
purujit77 <purujit6351@gmail.com>
2022-11-08 12:09:35 +0530

    UI Enhancement

--- chart.js
+++ chart.js
@@ -1,35 +1,49 @@
 var xValues = [50,60,70,80,90,100,110,120,130,140,150];
 var yValues = [7,8,8,9,9,9,10,11,14,14,15];
+const ctx = document.getElementById("myChart").getContext("2d");
+
+let gradient = ctx.createLinearGradient(0,0,0,400);
+gradient.addColorStop(0, "rgba(58,123,213,1)");
+gradient.addColorStop(1,"rgba(0,210,255,0)");
 
 new Chart("myChart", {
   type: "line",
   data: {
     labels: xValues,
     datasets: [{
-      fill: false,
+      fill: true,
       lineTension: 0,
-      backgroundColor: "rgba(0,0,255,1.0)",
-      borderColor: "rgba(0,0,255,0.1)",
+      backgroundColor: gradient,
+      borderColor: "white",
       data: yValues
     }]
   },
   options: {
     legend: {display: false},
+    responsive: true,
+    // maintainAspectRatio: false,
     scales: {
       yAxes: [{
-        ticks: {min: 6, max:16},
+        ticks: {
+          min: 6, 
+          max:16, 
+          // fontColor: "white",
+        },
         scaleLabel: {
             display: true,
-            labelString: 'Prices'
+            labelString: 'Month',
+            // fontColor: "white",
         }
-      
       }],
       xAxes: [{
+        ticks: {min: 6, max:16,},
         scaleLabel: {
             display: true,
-            labelString: 'Month'
+            labelString: 'Month',
+            // fontColor: "white",
         }
       }]
     }
+    
   }
 });
